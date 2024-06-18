cdce138f354d1349f7e6bbb41be26a84e0c92f7a
jaedeokhan <hjaedeok@gmail.com>
2024-06-18 23:35:49 +0900

    Chore : 폴더 수정

--- src/main/java/org/example/sprinbatch/JobRunner.java
+++ src/main/java/org/example/sprinbatch/JobRunner.java
@@ -1,4 +1,4 @@
-package org.example.sprinbatch.sec03.e02;
+package org.example.sprinbatch;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
@@ -9,7 +9,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class JobRunner implements ApplicationRunner {
 
     @Autowired
