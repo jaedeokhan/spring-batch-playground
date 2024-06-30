6d90673426b7912295505cef17b7c8dd77399c3a
jaedeokhan <hjaedeok@gmail.com>
2024-06-30 23:49:49 +0900

    Chore : StepExecutionContext, JobExecutionContext는 Step 간 공유가 가능하고, StepExecutionContext는 Step간 서로 공유 불가능

--- src/main/java/org/example/sprinbatch/ExecutionContextConfiguration.java
+++ src/main/java/org/example/sprinbatch/ExecutionContextConfiguration.java
@@ -14,40 +14,50 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobSample {
+public class ExecutionContextConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final ExecutionContextTasklet1 executionContextTasklet1;
+    private final ExecutionContextTasklet2 executionContextTasklet2;
+    private final ExecutionContextTasklet3 executionContextTasklet3;
+    private final ExecutionContextTasklet4 executionContextTasklet4;
 
     @Bean
     public Job job() {
         return jobBuilderFactory.get("job")
                 .start(step1())
                 .next(step2())
+                .next(step3())
+                .next(step4())
                 .build();
     }
 
     @Bean
     public Step step1() {
         return stepBuilderFactory.get("step1")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step1 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+                .tasklet(executionContextTasklet1)
+                .build();
     }
 
     @Bean
     public Step step2() {
         return stepBuilderFactory.get("step2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step2 has executed");
-                        return RepeatStatus.FINISHED;
-                    }
-                }).build();
+                .tasklet(executionContextTasklet2)
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
+                .tasklet(executionContextTasklet3)
+                .build();
+    }
+
+    @Bean
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
+                .tasklet(executionContextTasklet4)
+                .build();
     }
 }
---
+++ src/main/java/org/example/sprinbatch/ExecutionContextTasklet4.java
@@ -0,0 +1,22 @@
+package org.example.sprinbatch;
+
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ExecutionContextTasklet4 implements Tasklet {
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        System.out.println("=== step 4 was stated ===");
+
+        Object name = chunkContext.getStepContext().getStepExecution().getJobExecution().getExecutionContext().get("name");
+
+        System.out.println("name : " + name);
+
+        return RepeatStatus.FINISHED;
+    }
+}
