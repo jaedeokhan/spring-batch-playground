7601de9b935cf6c853c0ddd9372f7c1fe87453cd
jaedeokhan <hjaedeok@gmail.com>
2024-07-07 17:10:30 +0900

    chore : 배치 초기화 설정 - BatchAutoConfiguation, BatchProperties

--- src/main/java/org/example/sprinbatch/JobConfiguration2.java
+++ src/main/java/org/example/sprinbatch/JobConfiguration2.java
@@ -14,38 +14,38 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-public class JobSample {
+public class JobConfiguration2 {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
     @Bean
-    public Job job() {
-        return jobBuilderFactory.get("job")
-                .start(step1())
-                .next(step2())
+    public Job job2() {
+        return jobBuilderFactory.get("job2")
+                .start(step3())
+                .next(step4())
                 .build();
     }
 
     @Bean
-    public Step step1() {
-        return stepBuilderFactory.get("step1")
+    public Step step3() {
+        return stepBuilderFactory.get("step3")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step1 has executed");
+                        System.out.println(">> step3 has executed");
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
     }
 
     @Bean
-    public Step step2() {
-        return stepBuilderFactory.get("step2")
+    public Step step4() {
+        return stepBuilderFactory.get("step4")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println(">> step2 has executed");
+                        System.out.println(">> step4 has executed");
                         return RepeatStatus.FINISHED;
                     }
                 }).build();
